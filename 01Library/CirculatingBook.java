28013b6a4758b6c6ceb3b46739b4374eace8e33b
Winnie Chen <wchen10@stuy.edu>
2016-12-05 04:42:47 -0500

    copied over almost finished code from notes on a device not connected to github (all code but toString()), finished toString() method, and chenged .equals(null) to == null

--- 01Library/CirculatingBook.java
+++ 01Library/CirculatingBook.java
@@ -1,3 +1,58 @@
-class ReferenceBook extends LibraryBook{
+class CirculatingBook extends LibraryBook{
+    private String currentHolder;
+    private String dueDate;
 
+    public CirculatingBook(String Author, String Title, String ISBNum, String CallNumber){
+	super(Author, Title, ISBNum,CallNumber);
+	currentHolder = null;
+	dueDate = null;
+    }
+
+    public String getCurrentHolder(){
+	return currentHolder;
+    }
+
+    public String getDueDate(){
+	return dueDate;
+    }
+
+    public boolean setCurrentHolder(String CurrentHolder){
+	boolean successful = false;
+	currentHolder = CurrentHolder;
+	successful = true;
+	return successful;
+    }
+
+    public boolean setDueDate(String DueDate){
+	boolean successful = false;
+	dueDate = DueDate;
+	successful = true;
+	return successful;
+    }
+
+    public void checkout(String patron, String due){
+	setCurrentHolder(patron);
+	setDueDate(due);
+    }
+
+    public void returned(){
+	setCurrentHolder(null);
+	setDueDate(null);
+    }
+
+    public String circulationStatus(){
+	if(getCurrentHolder() == null){
+	    return "book available on shelves";
+	} else{
+	    return getCurrentHolder() + "will return the book by " + getDueDate();
+	}
+    }
+
+    public String toString(){
+	if(getCurrentHolder() == null){
+	    return super.toString();
+	} else{
+	    return super.toString() + ", " + getCurrentHolder() + ", " + getDueDate();
+	}
+    }
 }
