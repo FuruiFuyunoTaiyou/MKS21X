3dd94e254c2d69538a23395fb53a3d74a2b58108
Winnie Chen <wchen10@stuy.edu>
2016-12-09 04:55:20 -0500

    Added toZip(String code), but did not test thoroughly

--- 03Barcode/Barcode.java
+++ 03Barcode/Barcode.java
@@ -14,6 +14,15 @@ public class Barcode implements Comparable<Barcode>{
 	return true;
     }
 
+    /*private boolean barsOnly(String barcode){
+	for(int i = 0; i < 32; i++){
+	    if(barcode.charAt(i) != '|' && barcode.charAt(i) != ':'){
+		return false;
+	    }
+	}
+	return true;
+	}*/
+
     private int checkSum(){
 	int sum = 0;
 	for(int i = 0; i < 5; i++){
@@ -52,9 +61,60 @@ public class Barcode implements Comparable<Barcode>{
 	return barcode;
     }
 
-    /*public static String toZip(String barcode){
-
-      }*/
+    public static String toZip(String barcode){
+	if(barcode.length() != 32){
+	    throw new IllegalArgumentException("barcode is not 32 characters long");
+	}else if(barcode.charAt(0) != '|'){
+	    throw new IllegalArgumentException("barcode does not start with |");
+	}else if(barcode.charAt(31) != '|'){
+	    throw new IllegalArgumentException("barcode does not end  with |");
+	}
+	for(int i = 0; i < 32; i++){
+	    if(barcode.charAt(i) != '|' && barcode.charAt(i) != ':'){
+		throw new IllegalArgumentException("barcode must only contain bars (i.e. | or :)");
+	    }
+	}
+	String zipAndCheck = "";
+	String[] code = {
+	    "||:::",
+	    ":::||", 
+	    "::|:|",
+	    "::||:",
+	    ":|::|",
+	    ":|:|:",
+	    ":||::",
+	    "|:::|",
+	    "|::|:",
+	    "|:|::"
+	};
+	//find zipAndCheck
+	boolean found = false;
+	for(int i = 1; i < 30; i += 5){
+	    found = false;
+	    for(int index = 0; index < 10; index++){
+		//System.out.println(index);
+		//System.out.println(barcode.substring(i, i + 5));
+		//System.out.println(code[index]);
+		if(barcode.substring(i, i + 5).equals(code[index])){
+		    zipAndCheck += index;
+		    found = true;
+		}
+	    }
+	    if(!found){
+		throw new IllegalArgumentException("barcode contains nonsense code");
+	    }
+	}
+	//verify validity of check sum
+	int sum = 0;
+	for(int i = 0; i < 5; i++){
+	    sum += Character.getNumericValue(zipAndCheck.charAt(i));
+	}
+        sum %= 10;
+	if(sum != Character.getNumericValue(zipAndCheck.charAt(5))){
+	    throw new IllegalArgumentException("barcode contains invalid check sum or invalid zip");
+	}
+	return zipAndCheck.substring(0, 5);
+    }
 
     public Barcode(String zip){
 	//System.out.println(zip.length());
@@ -93,5 +153,6 @@ public class Barcode implements Comparable<Barcode>{
         Barcode bars = new Barcode("01234");
 	System.out.println(bars);
 	System.out.println(bars.compareTo(new Barcode("12345")));
+	System.out.println(toZip(toCode("01234")));
     }
 }
