a242fb7b1a34d7b6dccce6a6a2d7a3e9d622f83d
Winnie Chen <wchen10@stuy.edu>
2016-12-16 06:07:13 -0500

    added if statements to deal with empty arrays. Still need to add BubbleSort(int[] data) method and method(s) to check runtimes.

--- 04Sorts/Sorts.java
+++ 04Sorts/Sorts.java
@@ -1,3 +1,6 @@
+import java.io.*;
+
+//the idea of using an if statement to account for empty arrays was form Jacqueline Xu
 public class Sorts{
     public static String name(){
 	return "10.Chen.Winnie";
@@ -13,15 +16,18 @@ public class Sorts{
 	//find the index of smallest number remaining
 	//swap places with the element in the place it's supposed to be in
 	//repeat
-	int indexOfCurrentSmallest = 0;
-	for (int i = 0; i < data.length - 1; i++){ //for each element in data except the last one,
-	    indexOfCurrentSmallest = i; //first set the  indexOfCurrentSmallest
-	    for (int index = i + 1; index < data.length; index++){ //run through the rest of the array to find the actual indexOfCurrentSmallest
-		if(data[index] < data[indexOfCurrentSmallest]){
-		    indexOfCurrentSmallest = index;
+
+	if(data.length > 1){
+	    int indexOfCurrentSmallest = 0;
+	    for (int i = 0; i < data.length - 1; i++){ //for each element in data except the last one,
+		indexOfCurrentSmallest = i; //first set the  indexOfCurrentSmallest
+		for (int index = i + 1; index < data.length; index++){ //run through the rest of the array to find the actual indexOfCurrentSmallest
+		    if(data[index] < data[indexOfCurrentSmallest]){
+			indexOfCurrentSmallest = index;
+		    }
 		}
+		swap(data, i, indexOfCurrentSmallest);
 	    }
-	    swap(data, i, indexOfCurrentSmallest);
 	}
     }
 
@@ -29,26 +35,32 @@ public class Sorts{
 	//compare the first element after the sorted subarray to each element from greatest to least
 	//if the value to be placed is less than the value it is being compared with, switch places
 	//if otherwise, you're done with placing that value; start over
-	int lostValue = 0;
-	for(int i = 1; i < data.length; i++){//for each value besides first one,
-	    lostValue = data[i];
-	    //System.out.println(lostValue);
-	    //System.out.println(i);
-	    int index = i - 1;
-	    //System.out.println(index);
-	    while(index >= 0 && lostValue < data[index]){
-		data[index + 1] = data[index];
-		data[index] = lostValue;
-		index--;
+
+	if(data.length > 1){
+	    int lostValue = 0;
+	    for(int i = 1; i < data.length; i++){//for each value besides first one,
+		lostValue = data[i];
+		//System.out.println(lostValue);
+		//System.out.println(i);
+		int index = i - 1;
+		//System.out.println(index);
+		while(index >= 0 && lostValue < data[index]){
+		    data[index + 1] = data[index];
+		    data[index] = lostValue;
+		    index--;
+		}
+		data[index + 1] = lostValue;
 	    }
-	    data[index + 1] = lostValue;
 	}
     }
 
     public static void printArray(int[] data){
-	String arrayValues = "[" + data[0];
-	for(int i = 1; i < data.length; i++){
-	    arrayValues += ", " + data[i];
+	String arrayValues = "[";
+	if(data.length > 0){
+	    arrayValues += data[0];
+	    for(int i = 1; i < data.length; i++){
+		arrayValues += ", " + data[i];
+	    }
 	}
 	arrayValues += "]";
 	System.out.println(arrayValues);
@@ -66,7 +78,7 @@ public class Sorts{
 	printArray(data1);
 	insertionSort(data2);
 	printArray(data2);
-	insertionSort(data3);
+        insertionSort(data3);
 	printArray(data3);
     }
 }
