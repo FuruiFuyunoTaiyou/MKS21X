ae99df12e863395f5ea400d468b5fffdeebaf3e4
Winnie Chen <wchen10@stuy.edu>
2016-12-18 19:55:48 -0500

    took out the import statement/cmd in beginning b/c not throwing exceptions anymore. Added and tested bubble sort. Still need to take out comments and add a testing method to test runtimes.

--- 04Sorts/Sorts.java
+++ 04Sorts/Sorts.java
@@ -1,5 +1,3 @@
-import java.io.*;
-
 //the idea of using an if statement to account for empty arrays was form Jacqueline Xu
 public class Sorts{
     public static String name(){
@@ -55,7 +53,27 @@ public class Sorts{
     }
 
     public static void bubbleSort(int[] data){
-	
+	//for each value (except the last), compare it with the next value. If the next value is smaller, swap places
+	//Repeat this for the array without the last element (because it is already in place) and so on if swaps have been made during the last pass
+	//if no swaps have been made for a whole pass, end the method (return ;)
+
+	boolean swapped = true;
+	if(data.length > 1){
+	    for(int end = data.length - 2; swapped && end >= 0; end--){ //shortening the unsorted array each time
+		swapped = false;
+		//System.out.println("end = " + end);
+		for(int i = 0; i <= end; i++){ //actually looping through and comparing for each shortened unsorted array
+		    //System.out.println("i = " + i);
+		    if(data[i] > data[i + 1]){
+			swap(data, i, i + 1);
+			swapped = true;
+		    }
+		}
+		if(!swapped){ //if no swaps/changes have occured by the end of a whole runthrough, 
+		    return ;
+		}
+	    }
+	}
     }
 
     public static void printArray(int[] data){
@@ -76,13 +94,13 @@ public class Sorts{
 	int[] data1 = {2, 0, 13, 0, 4, 1};
 	int[] data2 = {0};
 	int[] data3 = {};
-        insertionSort(data0);
+        bubbleSort(data0);
 	printArray(data0);
-	insertionSort(data1);
+        bubbleSort(data1);
 	printArray(data1);
-	insertionSort(data2);
+        bubbleSort(data2);
 	printArray(data2);
-        insertionSort(data3);
+        bubbleSort(data3);
 	printArray(data3);
     }
 }
